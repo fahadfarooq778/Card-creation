582a2e0234010dd3239a52f8126be4e04ecb3862
Fahad Farooq <farooqmuhammadfahad012@gmail.com>
2025-10-26 22:18:49 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+function changeMessage() {
+    document.getElementById("message").innerHTML = "You are the best teacher! 💐 We appreciate your hard work and love.";
+
+    document.getElementById("message").innerHTML = "🌷 Thank you, dear teachers, for your patience, kindness, and love. You are the reason we learn, grow, and dream big every day. 💖";
+
+
+
+}
